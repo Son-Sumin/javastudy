cf0d3b195550e85cb3e76db3baeedd70e9b69fd8
Son-Sumin <goldkitty007@gmail.com>
2022-10-27 17:36:13 +0900

    practice03 #3

--- practice03/src/main/java/prob03/CurrencyConverter.java
+++ practice03/src/main/java/prob03/CurrencyConverter.java
@@ -1,5 +1,29 @@
 package prob03;
 
+//import java.math.BigDecimal;
+
 public class CurrencyConverter {
- 
+	private static double rate;
+
+	public static double toDollar(double won) {
+//		BigDecimal W = new BigDecimal(won);
+//		BigDecimal R = new BigDecimal(rate);
+//		return W.divide(R, 1, BigDecimal.ROUND_DOWN);
+		
+		return (long)(won / rate);
+		
+	}
+
+	public static double toKRW(double dollar) {
+//		BigDecimal D = new BigDecimal(dollar);
+//		BigDecimal R = new BigDecimal(rate);
+//		return D.multiply(R);
+		
+		return (float)(dollar * rate);
+	}
+
+	public static void setRate(double r) {
+		// 환율 설정(KRW/$1)
+	}
+
 }
