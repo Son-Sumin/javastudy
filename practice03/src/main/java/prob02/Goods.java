0d27de8ebc54c291847134090774a86410c3fa4d
Son-Sumin <goldkitty007@gmail.com>
2022-10-27 15:49:01 +0900

    practice03 #2

--- practice03/src/main/java/prob02/Goods.java
+++ practice03/src/main/java/prob02/Goods.java
@@ -5,7 +5,13 @@ public class Goods {
 	private int price;
 	private int countStock;
 	
+	public Goods(String n, int p, int c) {
+		this.name = n;
+		this.price = p;
+		this.countStock = c;
+	}
+	
 	public void showInfo() { 
-		System.out.println(name + "(가격: " + price +"원)이" + countStock + "개 입고 되었습니다.");
+		System.out.println(name + "(가격: " + price +"원)이 " + countStock + "개 입고 되었습니다.");
 	}
 }
--- practice03/src/main/java/prob02/GoodsApp.java
+++ practice03/src/main/java/prob02/GoodsApp.java
@@ -12,16 +12,21 @@ public class GoodsApp {
 		Goods[] goods = new Goods[COUNT_GOODS];
 
 		// 상품 입력
-		String line = scanner.nextLine();
-		String[] infos = line.split(" ");
-		// System.out.println(Arrays.toString(infos));
+		for(int i = 0; i < 3; i++) {
+			String line = scanner.nextLine();
+			String[] infos = line.split(" ");
+			
+			String name = infos[0];
+			int price = Integer.parseInt(infos[1]);
+			int countStock = Integer.parseInt(infos[2]);
+			
+			goods[i] = new Goods(infos[0], Integer.parseInt(infos[1]), Integer.parseInt(infos[2]));
+		}
 		
-		String name = infos[0];
-		int price = Integer.parseInt(infos[1]);
-		int countStock = Integer.parseInt(infos[2]);
-
 		// 상품 출력
-		//goods.showInfo();
+		goods[0].showInfo();
+		goods[1].showInfo();
+		goods[2].showInfo();
 		
 		// 자원정리
 		scanner.close();
