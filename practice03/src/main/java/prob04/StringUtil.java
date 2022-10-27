67c3c43909324b1835bfa79e9db01a10776f843f
Son-Sumin <goldkitty007@gmail.com>
2022-10-27 21:49:58 +0900

    prac #04

--- practice03/src/main/java/prob04/StringUtil.java
+++ practice03/src/main/java/prob04/StringUtil.java
@@ -1,8 +1,16 @@
 package prob04;
 
 public class StringUtil {
-	public static String concatenate(String[] str) {
-        //문자열을 결합하여 리턴 하는 메소드 구현
+    public static String concatenate(String[] str) {
+    	//String result = str.toString();
+    	String[] result = new String[1];
+        for(int i = 0; i < str.length; i++) {
+        	String[] r = new String[str.length];
+        	r[i] += str[i];
+        	System.out.println(r[i]);
+        }
+        return null;
     }
+    
 
 }
