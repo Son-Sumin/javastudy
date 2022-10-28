2b9f8639ffe0ffb702d732195e27049cd3c8c11a
Son-Sumin <goldkitty007@gmail.com>
2022-10-28 19:35:07 +0900

    practice03 #4

--- practice03/src/main/java/prob04/StringUtil.java
+++ practice03/src/main/java/prob04/StringUtil.java
@@ -1,18 +1,13 @@
 package prob04;
 
 public class StringUtil {
-    public static String concatenate(String[] str) {
+	public static String concatenate(String[] str) {
     	
-    	String[] result = new String[str.length];
+    	String result = "";
         for(int i = 0; i < str.length; i++) {
-//        	if (str[i] == null) {
-//        		System.out.println("");
-//        	}
-        	result[str.length-1] += str[i];
-        	//System.out.print(result[str.length-1]);
+        	result+= str[i];
         }
-        return result[str.length-1];
+        return result;
     }
-    
 
 }
