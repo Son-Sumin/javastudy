7d49ab9bbcade07ea99bbd621082cde02cb2fcb6
Son-Sumin <goldkitty007@gmail.com>
2022-10-28 17:40:14 +0900

    practice04 #05

--- practice04/src/main/java/prob05/MyBase.java
+++ practice04/src/main/java/prob05/MyBase.java
@@ -2,6 +2,15 @@ package prob05;
 
 public class MyBase extends Base {
 
+	public void service(String s) {
+		if (s.equals("낮")) {
+			System.out.println("낮에는 열심히 일하자!");
+		} else if (s.equals("밤")) {
+			System.out.println("night");
+		} else {
+			System.out.println("오후도 낮과 마찬가지로 일해야 합니다.");
+		}
+	}
 }
 
 // Base에서 override해서 코드생성해라
