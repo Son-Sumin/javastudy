fdb366dfd0dafa36559786efb443ef55ee111d71
Son-Sumin <goldkitty007@gmail.com>
2022-10-28 12:29:20 +0900

    practice04 #03

--- practice04/src/main/java/prob03/DropShip.java
+++ practice04/src/main/java/prob03/DropShip.java
@@ -1,23 +1,25 @@
 package prob03;
 
-public class DropShip{
+public class DropShip extends Unit {
 	// 현재 위치
 	private int x;
 	private int y;
-
-	void move(int x, int y) {
+	
+	@ Override
+	public void move(int x, int y) {
 		/* 지정된 위치로 이동 */
 	}
 
-	void stop() {
+	@ Override
+	public void stop() {
 		/* 현재 위치에 정지 */
 	}
 	
-	void load() {
+	public void load() {
 		/* 선택된 대상을 태운다.*/ 
 	}
 	
-	void unload() {
+	public void unload() {
 		/* 선택된 대상을 내린다.*/ 
 	}
 }
--- practice04/src/main/java/prob03/Tank.java
+++ practice04/src/main/java/prob03/Tank.java
@@ -1,19 +1,21 @@
 package prob03;
 
-public class Tank{
+public class Tank extends Unit{
 	// 현재 위치
 	private int x;
 	private int y;
 
-	void move(int x, int y) {
+	@ Override
+	public void move(int x, int y) {
 		/* 지정된 위치로 이동 */
 	}
 
-	void stop() {
+	@ Override
+	public void stop() {
 		/* 현재 위치에 정지 */
 	}
-	
-	void changeMode() {
+
+	public void changeMode() {
 		/* 공격모드를 변환한다. */
 	}
 }
