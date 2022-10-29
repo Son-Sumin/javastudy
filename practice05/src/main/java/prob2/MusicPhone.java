946044e601e92e87f1df48b3b1f80cd7fba02d2d
Son-Sumin <goldkitty007@gmail.com>
2022-10-30 04:52:45 +0900

    practice05 #2

--- practice05/src/main/java/prob2/MusicPhone.java
+++ practice05/src/main/java/prob2/MusicPhone.java
@@ -2,17 +2,17 @@ package prob2;
 
 public class MusicPhone extends Phone {
 	@Override
-	public void execute ( String function ) {
-	      if (function.equals("음악") ) {   // string 비교는 = 이 아니라 equals
-	          playMusic();
-	          return;
-	      }
-	      
-	      super.execute(function);
+	public void execute(String function) {
+		if (function.equals("음악")) { // string 비교는 = 이 아니라 equals
+			playMusic();
+			return;
+		}
+
+		super.execute(function);
 	}
 
-	  private void playMusic(){
-	    System.out.println("MP3 플레이어에서 음악재생");
-	  }
+	private void playMusic() {
+		System.out.println("MP3 플레이어에서 음악재생");
+	}
 
 }
