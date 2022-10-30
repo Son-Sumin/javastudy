0cc6bbe2d2ef85c6fb9bc13a434ba491e5dc6504
Son-Sumin <goldkitty007@gmail.com>
2022-10-30 16:01:14 +0900

    .

---
+++ practice05/src/main/java/prob3/Duck.java
@@ -0,0 +1,30 @@
+package prob3;
+
+public class Duck extends Bird {
+	private String name ;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sing() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void show() {
+		System.out.println(toString());
+	}
+
+}
