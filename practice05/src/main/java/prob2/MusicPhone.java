011be127271cf762b1974fe7440cafe56d505e92
Son-Sumin <goldkitty007@gmail.com>
2022-10-30 15:53:37 +0900

    .

--- practice05/src/main/java/prob2/MusicPhone.java
+++ practice05/src/main/java/prob2/MusicPhone.java
@@ -16,15 +16,6 @@ public class MusicPhone extends Phone {
 		System.out.println("MP3 플레이어에서 음악재생");
 	}
 
-//	@Override
-//	public void execute (String function) {
-//		 if (function.equals("앱")) {
-//			 playApp();
-//			 return;
-//		 }
-//		 super.execute(function);
-//	}
-
 }
 
 // string 비교는 = 이 아니라 equals
