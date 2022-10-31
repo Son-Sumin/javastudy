96a57fb2e3bc9b835e960ca3911befc04b50a6a2
Son-Sumin <goldkitty007@gmail.com>
2022-10-31 10:50:58 +0900

    HashCode + equals #1

---
+++ chapter04/src/main/java/chapter04/HashSetTest.java
@@ -0,0 +1,27 @@
+package chapter04;
+
+public class HashSetTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/*
+ * 자바 제공 자료구조 클래스 => collection Frameworks
+ *   => 객체 동일 여부가 아니라 내용을 봐야함. 자료구조는 내용 비교!!
+ *   
+ * List, Map, Set, Stack, ...
+ * HashSet
+ *    1. 순서 여부
+ *    2. 중복 여부
+ *    - list(순서O, 중복O)  -> 자료 가져올 때 순서 활용
+ *    - set(순서X, 중복X)  -> 자료 가져올 때 순서가 없으니 for문 활용
+ *    - map(key와 value가 존재하여 일대일대응)  -> 자료 가져올 때 key 활용
+ *    
+ * 
+ * 
+ * 
+ */
