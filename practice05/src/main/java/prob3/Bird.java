2aed544b573ca59a3b59f77db96124dde67e792c
Son-Sumin <goldkitty007@gmail.com>
2022-10-31 19:30:29 +0900

    practice05 #3

--- practice05/src/main/java/prob3/Bird.java
+++ practice05/src/main/java/prob3/Bird.java
@@ -13,4 +13,8 @@ public abstract class Bird {
 	
 	public abstract void fly();
 	public abstract void sing();
+	
+	public String toString() {
+		return "";
+	}
 }
--- practice05/src/main/java/prob3/Duck.java
+++ practice05/src/main/java/prob3/Duck.java
@@ -3,28 +3,31 @@ package prob3;
 public class Duck extends Bird {
 	private String name ;
 	
+	@Override
 	public String getName() {
 		return name;
 	}
 
+	@Override
 	public void setName(String name) {
 		this.name = name;
 	}
 
 	@Override
 	public void fly() {
-		// TODO Auto-generated method stub
+		System.out.println("오리(" + name + ")는 날지 않습니다.");
 
 	}
 
 	@Override
 	public void sing() {
-		// TODO Auto-generated method stub
+		System.out.println("오리(" + name + ")는 소리내어 웁니다.");
 
 	}
 	
-	public void show() {
-		System.out.println(toString());
+	@Override
+	public String toString() {
+		return "오리의 이름은 " + this.name + " 입니다.";
 	}
 
 }
