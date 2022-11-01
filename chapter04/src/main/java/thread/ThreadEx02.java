a5b91489b313279cc7114d6262d3bd30d5ccf827
Son-Sumin <goldkitty007@gmail.com>
2022-11-01 11:19:16 +0900

    thread

--- chapter04/src/main/java/thread/ThreadEx02.java
+++ chapter04/src/main/java/thread/ThreadEx02.java
@@ -16,7 +16,9 @@ public class ThreadEx02 {
 
 }
 
-/* main(this sheet)는 실행 후 끌
- * t1 실행하다 lock, t2 실행하다 lock 번갈아가면서 함
- * 
+/*
+ *  main(this sheet)는 실행 후 끌
+ * t1 실행하다 lock, t2 실행하다 lock 번갈아가면서 실행되지만 순서 예측 불가
+ * 그래서 synchronized 통해 제어하지 못하는 자원과 thread들의 진행을 동기화시킴
+ * 현재 진행 중인 thread 제외하고 나머지 thread가 접근할 수 없게 막음
  */
