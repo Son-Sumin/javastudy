8e82e245e4b307c45631afd21f39d9db0db82ddf
Son-Sumin <goldkitty007@gmail.com>
2022-11-02 13:45:13 +0900

    BufferedReader

---
+++ chapter04/src/main/java/io/BufferedReaderTest.java
@@ -0,0 +1,42 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderTest {
+
+	public static void main(String[] args) {
+		BufferedReader br = null;
+
+		// 기반 스트림
+		// linux에서는 /(표준), window에서는 \를 /으로 만들어줌, 드라이브 붙이지 말기
+		try {
+			FileReader fr = new FileReader("./src/main/java/io/BufferedReaderTest.java");
+
+			// 보조 스트림
+			br = new BufferedReader(fr);
+
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Can Not Open: " + e);
+		} catch (IOException e) {
+			System.out.println("Can Not Open: " + e);
+		} finally {
+			try {
+				if (br != null) {
+					br.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+}
