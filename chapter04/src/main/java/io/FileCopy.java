89d011b39dcbb4555ce8dc6def5b606371dbb309
Son-Sumin <goldkitty007@gmail.com>
2022-11-02 11:05:10 +0900

    io #2

---
+++ chapter04/src/main/java/io/FileCopy.java
@@ -0,0 +1,44 @@
+package io;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FileCopy {
+
+	public static void main(String[] args) {
+		InputStream is = null;
+		OutputStream os = null;
+		
+		try {
+			is = new FileInputStream("맹구.jpg");
+			os = new FileOutputStream("맹구copy.jpg");
+			
+			int data = -1;  // data를 다 읽을면 -1임
+			
+			while((data = is.read()) != -1) {
+				os.write(data);
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("file not found");
+		} catch (IOException e) {
+			System.out.println("error: " + e);
+		}
+		finally {
+			try {
+				if(is != null) {
+					is.close();	
+				} if(os != null) {
+					os.close();	
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		
+			}
+		}
+	}
+}
