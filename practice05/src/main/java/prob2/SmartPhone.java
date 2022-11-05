2c3aaf738ed89beb3bbd9495a55b3a9483194592
Son-Sumin <goldkitty007@gmail.com>
2022-11-05 16:06:14 +0900

    앱실행 확인중

--- practice05/src/main/java/prob2/SmartPhone.java
+++ practice05/src/main/java/prob2/SmartPhone.java
@@ -14,5 +14,23 @@ public class SmartPhone extends MusicPhone {
 	public void playApp() {
 		System.out.println("앱실행");
 	}
+/*
+	@Override
+	public void execute(String function) {
+		if ("앱".equals(function)) {
+			runApp();
+			return;
+		}
+		super.execute(function);
+	}
 
+	@Override
+	protected void playMusic() {
+		System.out.println("다운로드해서 음악 재생");
+	}
+
+	protected void runApp() {
+		System.out.println("앱 실행");
+	}
+*/
 }
