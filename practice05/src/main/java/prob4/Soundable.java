f329a03b7ded9a8b1a5f31833b189a6841fb1f2f
Son-Sumin <goldkitty007@gmail.com>
2022-11-06 09:02:20 +0900

    interface Soundable 확인

--- practice05/src/main/java/prob4/Soundable.java
+++ practice05/src/main/java/prob4/Soundable.java
@@ -1,7 +1,7 @@
 package prob4;
 
 public interface Soundable {
-	String sound();
-	// = public String sound();
+	public String sound();
+	// = public sound();
 	//  interface는 다 public이기에 public void 또는 void 가능
 }
